c2d15a7f649b5c7dda4415e122ed0ac34294016e
jinwenma10 <jinwen.ma10@rafflesgirlssch.edu.sg>
2026-07-04 10:06:48 +0800

    Initial Commit

---
+++ screen-app/screen_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  screen_appApp.swift
+//  screen-app
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screen_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
